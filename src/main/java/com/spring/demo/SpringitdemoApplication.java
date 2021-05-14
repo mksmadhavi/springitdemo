f9b170d6e89e7eead6ee5c96a0a762e4ab74834e
dev04 <dev04@DESKTOP-J1EFS11.Dlink>
2021-05-14 16:07:57 +0530

    first commit

---
+++ src/main/java/com/spring/demo/SpringitdemoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringitdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringitdemoApplication.class, args);
+	}
+
+}
